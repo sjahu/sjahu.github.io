406753168346de8da5accf8cf726c0ad9ac45047
sjahu <sjahu@users.noreply.github.com>
2022-10-05 16:55:04 +0000

    deploy: a02b76248d5456c59778f95631a813f2be17dc72

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#/bin/zsh
-
-# Run this script from the root directory of the repository
-
-echo "Installing pre-commit hook"
-rm -f .git/hooks/pre-commit
-ln -s ../../util/hooks/pre-commit .git/hooks/pre-commit
-
